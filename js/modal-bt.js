0605c21e332658fda8df6bee20b0a22b2463dff3
Sanya486 <120394126+Sanya486@users.noreply.github.com>
2023-02-21 20:59:18 +0200

    MODAL-FORM-TABLE

--- js/modal-bt.js
+++ js/modal-bt.js
@@ -5,11 +5,24 @@
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
     };
+
+    const disableBodyScroll = () => {
+      document.body.style.overflow = 'hidden';
+    }
+    
+    const enableBodyScroll = () => {
+      document.body.style.overflow = 'visible';
+    }
   
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
+      if (refs.modal.classList.contains("is-hidden")) {
+        enableBodyScroll();
+      } else {
+        disableBodyScroll(); 
+      }
     }
   })();
