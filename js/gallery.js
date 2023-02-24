2ff845a6f0cac9414eea96de010868c8b57159a1
Sanya486 <120394126+Sanya486@users.noreply.github.com>
2023-02-24 21:56:56 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -17,3 +17,59 @@ document.querySelector('.button-left').addEventListener('click', function () {
     }
     photoSlider.style.right = offset + 'px';
 });
+
+
+// let offset = 0;
+// const photoSlider = document.querySelector('.photo-slider');
+// const viewportWidth = window.innerWidth;
+
+// function adjustSlideWidth() {
+//   let slideWidth;
+//   if (viewportWidth >= 1200) {
+//     slideWidth = 298;
+//   } else if (viewportWidth >= 768) {
+//     slideWidth = 248;
+//   } else {
+//     slideWidth = 198;
+//   }
+//   return slideWidth;
+// }
+
+// function adjustOffset() {
+//   const slideWidth = adjustSlideWidth();
+//   const numSlides = Math.ceil(photoSlider.offsetWidth / slideWidth);
+//   const maxOffset = (numSlides - 1) * slideWidth;
+//   if (offset > maxOffset) {
+//     offset = maxOffset;
+//   }
+// }
+
+// function moveSlider(direction) {
+//   const slideWidth = adjustSlideWidth();
+//   adjustOffset();
+//   if (direction === 'right') {
+//     offset += slideWidth;
+//     if (offset > maxOffset) {
+//       offset = 0;
+//     }
+//   } else if (direction === 'left') {
+//     offset -= slideWidth;
+//     if (offset < 0) {
+//       offset = maxOffset;
+//     }
+//   }
+//   photoSlider.style.right = offset + 'px';
+// }
+
+// document.querySelector('.button-right').addEventListener('click', function () {
+//   moveSlider('right');
+// });
+
+// document.querySelector('.button-left').addEventListener('click', function () {
+//   moveSlider('left');
+// });
+
+// window.addEventListener('resize', function () {
+//   adjustOffset();
+//   moveSlider('none');
+// });
