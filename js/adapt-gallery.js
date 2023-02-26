f88872fa97e954a583758cec5fc185358081526b
Sanya486 <120394126+Sanya486@users.noreply.github.com>
2023-02-26 03:49:56 +0200

    Эксперименты со слайдером

--- js/adapt-gallery.js
+++ js/adapt-gallery.js
@@ -1,55 +1,55 @@
 let offset = 0;
+let gap = 32;
 const photoSlider = document.querySelector('.photo-slider');
 const viewportWidth = document.body.clientWidth;
-
-
+const buttonRight = document.querySelector('.button-right');
+const buttonLeft = document.querySelector('.button-left');
+const imgWidth = document.querySelector('.width-js').clientWidth;
 
 if (viewportWidth >= 1440) {
-    document.querySelector('.button-right').addEventListener('click', function (){
-        offset = offset + 298;
-        if (offset > 1490) {
-            offset = 0
-        }
-        photoSlider.style.right = offset + 'px';
-    });
-    
-    
-    document.querySelector('.button-left').addEventListener('click', function () {
-        offset = offset - 298;
-        if (offset < 0) {
-            offset = 0
-        }
-        photoSlider.style.right = offset + 'px';
-    }); 
+  buttonRight.addEventListener('click', function () {
+    offset = offset + 298;
+    if (offset > 1490) {
+      offset = 0;
+    }
+    photoSlider.style.right = offset + 'px';
+  });
 
+  buttonLeft.addEventListener('click', function () {
+    offset = offset - 298;
+    if (offset < 0) {
+      offset = 0;
+    }
+    photoSlider.style.right = offset + 'px';
+  });
 } else if (viewportWidth >= 768) {
-    document.querySelector('.button-right').addEventListener('click', function (){
-        offset = offset + 368;
-        if (offset > 3312) {
-            offset = 0
-        }
-        photoSlider.style.right = offset + 'px';
-    });
-    document.querySelector('.button-left').addEventListener('click', function () {
-        offset = offset - 368;
-        if (offset < 0) {
-            offset = 0
-        }
-        photoSlider.style.right = offset + 'px';
-    });
+  buttonRight.addEventListener('click', function () {
+    offset = offset + 398;
+    if (offset > 1592) {
+      offset = 0;
+    }
+    photoSlider.style.right = offset + 'px';
+  });
+  buttonLeft.addEventListener('click', function () {
+    offset = offset - 398;
+    if (offset < 0) {
+      offset = 0;
+    }
+    photoSlider.style.right = offset + 'px';
+  });
 } else {
-    document.querySelector('.button-right').addEventListener('click', function (){
-        offset = offset + 177;
-        if (offset > 1593) {
-            offset = 0
-        }
-        photoSlider.style.right = offset + 'px';
-    });
-    document.querySelector('.button-left').addEventListener('click', function () {
-        offset = offset - 177;
-        if (offset < 0) {
-            offset = 0
-        }
-        photoSlider.style.right = offset + 'px';
-    });
+  buttonRight.addEventListener('click', function () {
+    offset = offset + (imgWidth + gap);
+    if (offset > (imgWidth + gap)*3) {
+      offset = 0;
+    }
+    photoSlider.style.right = offset + 'px';
+  });
+  buttonLeft.addEventListener('click', function () {
+    offset =  offset - (imgWidth - gap);
+    if (offset < 0) {
+      offset = 0;
+    }
+    photoSlider.style.right = offset + 'px';
+  });
 }
